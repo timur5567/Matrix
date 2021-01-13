122a18fb93cf447f1fd441ee771c8058202e06dc
t.vagapov <75128238+timur5567@users.noreply.github.com>
2021-01-13 14:40:41 +0400

    init

---
+++ Matrix multiplication/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Matrix multiplication
+//
+//  Created by msoft on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
